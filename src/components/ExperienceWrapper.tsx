2b273590d75c2ec60d561a4ca6a92a7e45242c64
JaeMinBird <jaeminbird@gmail.com>
2025-06-29 22:45:37 -0400

    Revert "attempt"
    This reverts commit fc51ab1c39081c15a0dd6134843c5a9180a6f10c.

---
+++ src/components/ExperienceWrapper.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
+// Dynamically import the Experience component to avoid build issues
+const ExperienceComponent = dynamic(() => import('./experience'), {
+  ssr: false,
+  loading: () => (
+    <div className="bg-white" style={{ height: '600px' }}>
+      <div className="w-full h-full flex items-center justify-center">
+        <div className="text-gray-400">Loading experience...</div>
+      </div>
+    </div>
+  )
+});
+
+export default function ExperienceWrapper() {
+  return <ExperienceComponent />;
+} 
